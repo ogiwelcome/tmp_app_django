dcbd753606a1de988fb31f61558b5bfa989fbcc4
patron <ogi.fordevelopment@gmail.com>
2021-08-27 21:37:42 +0900

    2-2

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request, nickname, age):
-    result = 'your account: "'+nickname+'" ('+str(age)+').'
-    return HttpResponse(result)
+def index(request):
+    params = {
+        'title':'Hello/Index',
+        'msg':'This is a sample page',
+        'goto':'next',
+    }
+    return render(request, 'hello/index.html', params)
+def next(request):
+    params = {
+        'title':'Hello/Next',
+        'msg':'This is another page',
+        'goto':'index',
+    }
+    return render(request, 'hello/index.html', params)
