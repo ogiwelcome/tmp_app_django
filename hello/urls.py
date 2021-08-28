f86120655cc4f04309b9cd1b9195487823545d15
patron <ogi.fordevelopment@gmail.com>
2021-08-28 13:52:30 +0900

    3-4

--- hello/urls.py
+++ hello/urls.py
@@ -1,5 +1,11 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
+from .views import FriendList, FriendDetail
 urlpatterns = [
-    url(r'', views.index, name='index')
+    path('', views.index, name='index'),
+    path('create', views.create, name='create'),
+    path('edit/<int:num>', views.edit, name='edit'),
+    path('delete/<int:num>', views.delete, name='delete'),
+    path('list', FriendList.as_view()),
+    path('detail/<int:pk>', FriendDetail.as_view()),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,23 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Friend
-from django.db.models import QuerySet
+from .forms import FriendForm
+from django.views.generic import ListView
+from django.views.generic import DetailView
 
-def __new_str__(self):
-    result = ''
-    for item in self:
-        result+='<tr>'
-        for k in item:
-            result += '<td>'+str(k)+'='+str(item[k])+'</td>'
-        result+='</tr>'
-    return result
+class FriendList(ListView):
+    model = Friend
 
-QuerySet.__str__=__new_str__
+class FriendDetail(DetailView):
+    model = Friend
 
 def index(request):
-    data = Friend.objects.all().values('id', 'name', 'age')
+    data = Friend.objects.all()
     params = {
         'title':'Hello',
         'data':data,
     }
     return render(request, 'hello/index.html', params)
+
+def create(request):
+    if (request.method == 'POST'):
+        obj = Friend()
+        friend = FriendForm(request.POST, instance=obj)
+        friend.save()
+        return redirect(to='/hello')
+    params = {
+        'title':'Hello',
+        'form':FriendForm(),
+    }
+    return render(request, 'hello/create.html', params)
+
+def edit(request, num):
+    obj = Friend.objects.get(id=num)
+    if (request.method == 'POST'):
+        friend = FriendForm(request.POST, instance=obj)
+        friend.save()
+        return redirect(to='/hello')
+    params = {
+        'title':'Hello',
+        'id':num,
+        'form':FriendForm(instance=obj),
+    }
+    return render(request, 'hello/edit.html', params)
+
+def delete(request, num):
+    friend = Friend.objects.get(id=num)
+    if (request.method=='POST'):
+        friend.delete()
+        return redirect(to='/hello')
+    params = {
+        'title': 'Hello',
+        'id': num,
+        'obj': friend,
+    }
+    return render(request, 'hello/delete.html', params)
